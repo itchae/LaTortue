f10a42fa07a4b3cf7799d2840b69e45c78b44f8d
Lucille MOISE <lucille.moise@gmail.com>
2017-03-10 15:40:47 +0100

    Grille faite !

--- src/turtle/ihm/Grille.java
+++ src/turtle/ihm/Grille.java
@@ -1,10 +1,56 @@
 package turtle.ihm;
 
-public class Grille {
+import java.awt.Graphics;
+import javax.swing.JComponent;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/*import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;*/
 
+abstract class Grille extends JComponent {
+	
+	/*public static void main(String[] args) {
+
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run () {makeIt();}
+		});
+	}*/
+	 
+	
+	/*protected static void makeIt() {
+
+		JFrame frameGrille = new JFrame("Grille");
+		frameGrille.setPreferredSize(new Dimension(200,200));
+		frameGrille.add(new Grille(4, 5));
+		frameGrille.pack();
+		frameGrille.setVisible(true);
+		frameGrille.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}*/
+
+	private int nbligne;
+	private int nbcolonne;
+	
+	public Grille(int colonne, int ligne){
+		super();
+		this.nbcolonne = colonne;
+		this.nbligne = ligne;
 	}
+	
+	
+	@Override
+	public void paintComponent (Graphics graphics){
+		Graphics gra = graphics.create();
+		int xmax = (int) this.getSize().getWidth();
+		int ymax = (int) this.getSize().getHeight();
+		for (int i=0; i<nbcolonne;i++){		
+			int x = xmax/nbcolonne*i;
+			gra.drawLine(x, 0, x, ymax);
+		}
+		for (int j=0; j<nbligne ; j++){
+			int y = ymax/nbligne*j;
+			gra.drawLine(0, y, xmax, y);
+		}
+	}
+	
 
 }
